b275777ad4fff69da766fb7f7167ec0d4bec5898
RuiZhang <kwf8hq@virginia.edu>
2024-06-06 15:55:41 -0600

    server

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,9 @@
+// backend/models/User.js
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// backend/server.js
+const userRoutes = require('./routes/users');
+app.use('/api/users', userRoutes);
+
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
