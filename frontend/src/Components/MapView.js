34b11a5d7af07a4c0f6df1c406a46bf869e89010
nicknc410 <kim.n.j@wustl.edu>
2024-06-10 13:44:27 -0600

    done

--- frontend/src/Components/MapView.js
+++ frontend/src/Components/MapView.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer , TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import ReactLeafletKml from 'react-leaflet-kml'; 
 import { useEffect, useState } from 'react';
+
 
   
   
@@ -11,10 +12,11 @@ function MapView(props) {
     const [kml, setKml] = useState(null);
 
     useEffect(() => {
-        const kmlText = '/home/nicholas/Jumpster/Jumpster/data/missoula_valley_0_50_179m.kml';
+        const kmlText = '/missoula_valley_0_50_179m.kml';
         fetch(kmlText)
         .then((response) => response.text())
         .then((text) => {
+            console.log(text);
             const parser = new DOMParser();
             const kml = parser.parseFromString(text, 'text/xml');
             setKml(kml);
@@ -25,22 +27,18 @@ function MapView(props) {
 
     return (
         <div>
-            <h2>Child Component</h2>
-            <p>{props.message}</p> {/* Use the props */}
-            <p>The number is: {props.number}</p> {/* Use the props */}
-            <div style={{ height: '50vh', width: '100%' }}>
-            <MapContainer center={[47.0677868126, -113.8838378701]} zoom={13} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
-            <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {kml && <ReactLeafletKml kml={kml} />}
-            <Marker position={[51.505, -0.09]}>
-            <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-            </Marker>
-            </MapContainer>
+            <div>
+                <MapContainer
+                    style={{ height: "500px", width: "100%" }}
+                    zoom={17}
+                    center={[47.0677868126, -113.8838378701]}
+                >
+                    <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    {kml && <ReactLeafletKml kml={kml} />}
+                </MapContainer>
             </div>
             
         </div>
