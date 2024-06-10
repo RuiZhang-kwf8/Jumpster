3c1479fbec0181825148730bbaf680231d09ec2f
nicknc410 <kim.n.j@wustl.edu>
2024-06-10 14:05:06 -0600

    update model

--- backend/models/Simulations.js
+++ backend/models/Simulations.js
@@ -1,27 +1,11 @@
 const Mongoose = require('mongoose');
 
 const SimulationSchema = new Mongoose.Schema({
-    id: { type: Number, unique: true, required: true, default: 1 },
+    name: String,
     latitude: Number,
     longitude: Number,
-    time: { type: Date, default: Date.now },
-    tiffdata: { type: String }
+    time: { type: Date, default: Date.now }
 }, { timestamps: true });
 
-//auto increment id
-SimulationSchema.pre('save', function(next) {
-    const doc = this;
-    if (doc.isNew) {
-        Mongoose.model('Simulation', SimulationSchema).findOne({}, {}, { sort: { 'id': -1 } }, function(err, lastSimulation) {
-            if (err) {
-                return next(err);
-            }
-            doc.id = lastSimulation ? lastSimulation.id + 1 : 1;
-            next();
-        });
-    } else {
-        next();
-    }
-});
 
 module.exports = Mongoose.model('Simulation', SimulationSchema);
