b762c5587925cddcebcec8138828606ab4d4d618
RuiyuZhang.SD <134260297+RuiZhang-kwf8@users.noreply.github.com>
2024-06-12 15:58:43 -0600

    fix model

--- backend/models/Simulations.js
+++ backend/models/Simulations.js
@@ -4,12 +4,9 @@ const SimulationSchema = new Mongoose.Schema({
     name: String,
     latitude: Number,
     longitude: Number,
-<<<<<<< HEAD
-    time:  Date
-=======
+    time:  Date,
     outputFileName: String,
     time: { type: Date, default: Date.now }
->>>>>>> 27ec0f160b00a0f37a83db2b5c97539f23be45a2
 }, { timestamps: true });
 
 
