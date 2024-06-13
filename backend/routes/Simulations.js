db4198b96f074c99f6ede6844aae087f0d162cff
nicknc410 <kim.n.j@wustl.edu>
2024-06-13 10:40:19 -0600

    done with file system changes

--- backend/routes/Simulations.js
+++ backend/routes/Simulations.js
@@ -3,10 +3,103 @@ const Simulations = require('../models/Simulations');
 const router = express.Router();
 const fs = require('fs');
 const { exec } = require('child_process');
+const path = require('path');
+const util = require('util');
+const execPromise = util.promisify(exec);
+const AdmZip = require('adm-zip');
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+
+function listAllFiles(directoryPath) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(files);
+        });
+    });
+}
+
+function listKmlFiles(directoryPath) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                return reject(err);
+            }
+            const kmlFiles = files.filter(file => file.startsWith('output') && path.extname(file) === '.kml');
+            resolve(kmlFiles);
+        });
+    });
+}
+
+function listKmzFiles(directoryPath) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                return reject(err);
+            }
+            const kmzFiles = files.filter(file => path.extname(file) === '.kmz');
+            resolve(kmzFiles);
+        });
+    });
+}
+function listPdfFiles(directoryPath) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                return reject(err);
+            }
+            const pdfFiles = files.filter(file => path.extname(file) === '.pdf');
+            resolve(pdfFiles);
+        });
+    });
+}
+
+function unzipKmzFile(kmzFilePath, outputDirectory) {
+    return new Promise((resolve, reject) => {
+        try {
+            const zip = new AdmZip(kmzFilePath);
+            zip.extractAllTo(outputDirectory, true);
+            resolve();
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+function deleteFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
+}
+function moveFile(filePath, newDirectoryPath) {
+    return new Promise((resolve, reject) => {
+        // Ensure the new directory exists
+        fs.mkdir(newDirectoryPath, { recursive: true }, (err) => {
+            if (err) {
+                return reject(err);
+            }
+
+            const fileName = path.basename(filePath);
+            const newFilePath = path.join(newDirectoryPath, fileName);
+
+            fs.rename(filePath, newFilePath, (err) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
 
 
 router.get('/', async (req, res) => {
@@ -17,7 +110,7 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-
+        console.log('Current working directory:', process.cwd());
         const fetch = (await import('node-fetch')).default;
 
         const path = (await import('path')).default;
@@ -25,69 +118,50 @@ router.post('/', async (req, res) => {
         const southbound = Number(req.body.latitude) - .01;
         const eastbound = Number(req.body.longitude) + .01;
         const westbound = Number(req.body.longitude) - .01;
-        const command2 = `cd ~/Firelab/Windninja/build/src/fetch_dem && ./fetch_dem --bbox ${northbound} ${eastbound} ${southbound} ${westbound} --src gmted output.tif`;        
-        exec(command2, (error, stdout, stderr) => {
-            if (error) {
-                console.log("all bad"); 
-                console.error(`Error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
 
-                console.log("all bad"); 
-                console.error(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        }); 
-        console.log("all good"); 
-        await sleep(1000);
-        
-        const command = `WindNinja_cli ~/Jumpster/Jumpster/backend/routes/exampleElevfile.cfg --elevation_file ~/Firelab/Windninja/build/src/fetch_dem/output.tif --output_path ~/Jumpster/Jumpster/backend/outputs`;        
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
+        const command = `cd ~/Firelab/Windninja/build/src/fetch_dem && ./fetch_dem --bbox ${northbound} ${eastbound} ${southbound} ${westbound} --src gmted output.tif`;      
+        console.log("command: ", command); 
+        await execPromise(command);
 
-        await sleep(100);
+        const command2 = `WindNinja_cli ~/Jumpster/Jumpster/backend/routes/exampleElevfile.cfg --elevation_file ~/Firelab/Windninja/build/src/fetch_dem/output.tif --output_path ~/Jumpster/Jumpster/backend/outputs`;        
+        console.log("command: ", command2); 
+        await execPromise(command2);
 
-        const output = '';
-        const command3 = `cd ../outputs && source ./convert_kmz_to_kml.sh`;        
-        exec(command3, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            output = stdout.trim(); 
-        });
+        console.log("listKmzFiles"); 
+        const directoryPath = path.resolve('./outputs');
+        const outputDirectory = './kmls';
 
-        
-        await sleep(10000);
-        const command4 = `cd ~/Music/Jumpster/backend/outputs && . ./movekmltopublic.sh`; 
-        exec(command3, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            output = stdout.trim(); 
-        });
+        const kmzFiles = await listKmzFiles(directoryPath);
+        console.log('KMZ Files:', kmzFiles);
+
+        for (const kmzFile of kmzFiles) {
+            const kmzFilePath = path.join(directoryPath, kmzFile);
+            await unzipKmzFile(kmzFilePath, outputDirectory);
+            console.log(`Unzipped ${kmzFile} to ${outputDirectory}`);
+            await deleteFile(kmzFilePath); // Delete the kmz file after unzipping
+            console.log(`Deleted ${kmzFile}`);
+        }  
+
+        const kmlFiles = await listKmlFiles(outputDirectory);
+        console.log('KML Files:', kmlFiles);
+        const kmlFileName="" + kmlFiles[0];
+        for (const kmlFile of kmlFiles) {
+            await moveFile(path.join(outputDirectory, kmlFile), '../frontend/public/kml');
+        }
+
+        const pdfFiles = await listPdfFiles(directoryPath);
+        console.log('PDF Files:', pdfFiles);
+        const pdfFileName="" + pdfFiles[0];
+        for (const pdfFile of pdfFiles) {
+            await moveFile(path.join(directoryPath, pdfFile), '../frontend/public/pdf');
+        }
+
+        const allExtraFiles = await listAllFiles(outputDirectory);
+        console.log('All Extra Files:', allExtraFiles);
+        for (const extraFile of allExtraFiles) {
+            await deleteFile(path.join(outputDirectory, extraFile));
+        }
+
 
         
         // Save simulation data to database if needed
@@ -96,7 +170,8 @@ router.post('/', async (req, res) => {
             latitude: req.body.latitude,
             longitude: req.body.longitude,
             time: new Date(req.body.time),
-            outputFileName: output
+            outputKmlFileName: kmlFileName,
+            outputPdfFileName: pdfFileName
         });
         
         await newSimulation.save();
